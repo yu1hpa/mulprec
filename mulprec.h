d700ef631b5239734ddfaa5aa8647758baff85b8
yu1hpa <yu.1hpa@gmail.com>
2021-11-01 15:31:31 +0900

    feat: add prototype

---
+++ mulprec.h
@@ -0,0 +1,23 @@
+#define KETA 10
+
+struct Number {
+    int n[KETA];
+    int sign;
+};
+
+void clearByZero(struct Number *);
+void putSign(struct Number *);
+void setRnd(struct Number *, int);
+void dispNumber(struct Number *);
+void dispNumberZeroSuppress(struct Number *);
+void copyNumber(struct Number *, struct Number *);
+void getAbs(struct Number *, struct Number *);
+int isZero(struct Number *);
+int mulBy10(struct Number *, struct Number *);
+int divBy10(struct Number *, struct Number *);
+int setInt(struct Number *, int);
+int setSign(struct Number *, int);
+int getSign(struct Number *);
+int numComp(struct Number *, struct Number *);
+int add(struct Number *, struct Number *, struct Number *);
+int sub(struct Number *, struct Number *, struct Number *);
