0686bedbd04bf14dc26669e9191bab5719776fe1
yu1hpa <yu.1hpa@gmail.com>
2022-03-10 23:13:21 +0900

    feat: final

--- mulprec.c
+++ mulprec.c
@@ -10,7 +10,7 @@
 #define RADIX 1000000000
 #define RADIX_KETA (int)log10(RADIX)
 
-#define MEMO_SIZE 10000
+#define MEMO_SIZE 2000
 
 
 struct Number N1;
@@ -20,9 +20,10 @@ struct Number N2;
 struct Number ZERO;
 
 void initNumber(){
-    clearByZero(&N1); clearByZero(&N2);
-    setInt(&N1, 1);
-    setInt(&N2, 2);
+    setSign(&N1, 1);
+    setIntToZero(&N1, 1);
+    setSign(&N2, 1);
+    setIntToZero(&N2, 2);
 }
 
 // 配列をゼロで初期化
@@ -77,6 +78,11 @@ void dispNumberZeroSuppress(struct Number *a){
 void copyNumber(struct Number *a, struct Number *b){
     *b = *a;
 }
+
+void copyByZero(struct Number *a){
+    copyNumber(&ZERO, a);
+}
+
 
 /*
  * 第一引数の絶対値を取って
@@ -135,7 +141,7 @@ int divBy10(struct Number *a, struct Number *b){
 int setInt(struct Number *a, int x){
     int i, m;
     if (x < 0) {
-        a->sign = -1;
+        setSign(a, -1);
         x = -x;
     }
     for(i = 0; i < KETA; i++){
@@ -145,6 +151,18 @@ int setInt(struct Number *a, int x){
     }
     if (x != 0) return -1;
     else return 0;
+}
+
+int setIntToZero(struct Number *a, int x){
+    if (x > 9) {
+        return -1;
+    }
+    if (x < 0) {
+        setSign(a, -1);
+        x = -x;
+    }
+    a->n[0] = x;
+    return 0;
 }
 
 int getInt(struct Number *a, int *x){
@@ -208,17 +226,17 @@ int add(struct Number *a, struct Number *b, struct Number *c){
     uint_fast64_t d, e = 0;
 
     if (a->sign == 1 && b->sign == -1) {
-        struct Number d; clearByZero(&d);
+        struct Number d; copyByZero(&d);
         getAbs(b, &d);
         sub(a, &d, c);
         return 0;
     } else if (a->sign == -1 && b->sign == 1) {
-        struct Number d; clearByZero(&d);
+        struct Number d; copyByZero(&d);
         getAbs(a, &d);
         sub(b, &d, c);
         return 0;
     } else if (a->sign == -1 && b->sign == -1) {
-        struct Number d, e; clearByZero(&d); clearByZero(&e);
+        struct Number d, e; copyByZero(&d); copyByZero(&e);
         getAbs(a, &d); getAbs(b, &e);
         add(&d, &e, c);
         setSign(c, -1);
@@ -242,18 +260,18 @@ int sub(struct Number *a, struct Number *b, struct Number *c){
     int_fast64_t ai, bi, h = 0;
 
     if(a->sign == 1 && b->sign == -1) {
-        struct Number d; clearByZero(&d);
+        struct Number d; copyByZero(&d);
         getAbs(b, &d);
         if(add(a, &d, c) != 0) return -1;
         return 0;
     } else if (a->sign == -1 && b->sign == 1) {
-        struct Number d; clearByZero(&d);
+        struct Number d; copyByZero(&d);
         getAbs(a, &d);
         if(add(b, &d, c) != 0) return -1;
         setSign(c, -1);
         return 0;
     } else if (a->sign == -1 && b->sign == -1) {
-        struct Number d, e; clearByZero(&d); clearByZero(&e);
+        struct Number d, e; copyByZero(&d); copyByZero(&e);
         getAbs(a, &d); getAbs(b, &e);
         sub(&e, &d, c);
         return 0;
@@ -273,7 +291,7 @@ int sub(struct Number *a, struct Number *b, struct Number *c){
                 }
             }
         } else {
-            struct Number d; clearByZero(&d);
+            struct Number d; copyByZero(&d);
             sub(b, a, &d);
             d.sign = -1;
             copyNumber(&d, c);
@@ -287,26 +305,26 @@ int sub(struct Number *a, struct Number *b, struct Number *c){
  */
 int multiple(struct Number *a, struct Number *b, struct Number *c){
     if (getSign(a) == 1 && getSign(b) == -1) {
-        struct Number d; clearByZero(&d);
+        struct Number d; copyByZero(&d);
         getAbs(b, &d);
         multiple(a, &d, c);
         setSign(c, -1);
         return 0;
     } else if (getSign(a) == -1 && getSign(b) == 1) {
-        struct Number d; clearByZero(&d);
+        struct Number d; copyByZero(&d);
         getAbs(a, &d);
         multiple(b, &d, c);
         setSign(c, -1);
         return 0;
     } else if (getSign(a) == -1 && getSign(b) == -1) {
-        struct Number d, e; clearByZero(&d); clearByZero(&e);
+        struct Number d, e; copyByZero(&d); copyByZero(&e);
         getAbs(a, &d);
         getAbs(b, &e);
         multiple(&d, &e, c);
         return 0;
     }
-    struct Number d, e; clearByZero(&d); clearByZero(&e);
-    clearByZero(c); //[NOTE]: 一つ前のcをクリアする
+    struct Number d, e; copyByZero(&d); copyByZero(&e);
+    copyByZero(c); //[NOTE]: 一つ前のcをクリアする
     uint_fast64_t h = 0;
     for (int i = 0; i < KETA - 1; i++){
         h = 0;
@@ -319,7 +337,7 @@ int multiple(struct Number *a, struct Number *b, struct Number *c){
             copyNumber(&d, &e);
         }
         if (i == 0) copyNumber(&e, &d);
-        struct Number t; clearByZero(&t);
+        struct Number t; copyByZero(&t);
         add(&d, c, &t);
         copyNumber(&t, c);
     }
@@ -331,10 +349,9 @@ int multiple(struct Number *a, struct Number *b, struct Number *c){
  * a + 1 = b
  */
 int increment(struct Number *a, struct Number *b){
-    struct Number one; clearByZero(&one);
     int r;
-    setInt(&one, 1);
-    r = add(a, &one, b);
+    setIntToZero(&N1, 1);
+    r = add(a, &N1, b);
     return r;
 }
 
@@ -342,23 +359,23 @@ int increment(struct Number *a, struct Number *b){
  * a / b = c...d
  */
 int divide(struct Number *a, struct Number *b, struct Number *c, struct Number *d){
-    struct Number n, m; clearByZero(&n); clearByZero(&m);
+    struct Number n, m; copyByZero(&n); copyByZero(&m);
     copyNumber(a, &n);
     if (getSign(a) == 1 && getSign(b) == -1) {
-        struct Number p; clearByZero(&p);
+        struct Number p; copyByZero(&p);
         getAbs(b, &p);
         divide(a, &p, c, d);
         setSign(c, -1);
         return 0;
     } else if (getSign(a) == -1 && getSign(b) == 1) {
-        struct Number p; clearByZero(&p);
+        struct Number p; copyByZero(&p);
         getAbs(a, &p);
         divide(&p, b, c, d);
         setSign(c, -1);
         setSign(d, -1);
         return 0;
     } else if (getSign(a) == -1 && getSign(b) == -1) {
-        struct Number p, q; clearByZero(&p); clearByZero(&q);
+        struct Number p, q; copyByZero(&p); copyByZero(&q);
         getAbs(a, &p);
         getAbs(b, &q);
         divide(&p, &q, c, d);
@@ -372,7 +389,7 @@ int divide(struct Number *a, struct Number *b, struct Number *c, struct Number *
         sub(&n, b, &m);
         copyNumber(&m, &n);
 
-        clearByZero(&m);
+        copyByZero(&m);
         increment(c, &m);
         copyNumber(&m, c);
     }
@@ -380,16 +397,52 @@ int divide(struct Number *a, struct Number *b, struct Number *c, struct Number *
     return 0;
 }
 
+int fastdivide(struct Number *a, struct Number *b, struct Number *c, struct Number *d){
+    if (getSign(a) == 1 && getSign(b) == -1) {
+        struct Number p; copyByZero(&p);
+        getAbs(b, &p);
+        fastdivide(a, &p, c, d);
+        setSign(c, -1);
+        return 0;
+    } else if (getSign(a) == -1 && getSign(b) == 1) {
+        struct Number p; copyByZero(&p);
+        getAbs(a, &p);
+        fastdivide(&p, b, c, d);
+        setSign(c, -1);
+        setSign(d, -1);
+        return 0;
+    } else if (getSign(a) == -1 && getSign(b) == -1) {
+        struct Number p, q; copyByZero(&p); copyByZero(&q);
+        getAbs(a, &p);
+        getAbs(b, &q);
+        fastdivide(&p, &q, c, d);
+        setSign(d, -1);
+        return 0;
+    }
+    if (!isZero(b)) return -1;
+    uint_fast64_t t = 0;
+    uint_fast64_t h = 0;
+    uint_fast64_t _b = b->n[0];
+    for (int i = KETA - 1; i >= 0; i--) {
+        t = RADIX * h + a->n[i];
+        h = t % _b;
+        c->n[i] = (t - h) / _b;
+    }
+    struct Number tmp; copyByZero(&tmp);
+    setInt(&tmp, h);
+    copyNumber(&tmp, d);
+    return 0;
+}
+
 int power(struct Number *a, struct Number *b, struct Number *c){
     struct Number d, e, f;
-    clearByZero(&d); clearByZero(&e); clearByZero(&f);
-    struct Number one; clearByZero(&one);
-    setInt(&one, 1);
-    setInt(c, 1);
+    copyByZero(&d); copyByZero(&e); copyByZero(&f);
+    setIntToZero(&N1, 1);
+    setIntToZero(c, 1);
     if(!isZero(b)) {
         return 0;
     }
-    if (numComp(b, &one) == 0) {
+    if (numComp(b, &N1) == 0) {
         copyNumber(a, c);
         return 0;
     }
@@ -405,38 +458,166 @@ int power(struct Number *a, struct Number *b, struct Number *c){
 }
 
 int fastpower(struct Number *a, struct Number *b, struct Number *c){
-    struct Number one; clearByZero(&one);
-    setInt(&one, 1);
-    setInt(c, 1);
+    setIntToZero(&N1, 1);
+    setIntToZero(c, 1);
 
     if (!isZero(b)) {
-        return 0;
+        return -1;
     }
-    if (numComp(b, &one) == 0){
+    if (numComp(b, &N1) == 0){
         copyNumber(a, c);
-        return 0;
+        return -1;
     }
 
-    struct Number x, y, z, w;
-    clearByZero(&x); clearByZero(&y); clearByZero(&z); clearByZero(&w);
+    struct Number x, z, w;
+    copyByZero(&x); copyByZero(&z); copyByZero(&w);
     copyNumber(b, &x);
-    setInt(&y, 2);
-    divide(&x, &y, &z, &w);
+    setIntToZero(&N2, 2);
+    fastdivide(&x, &N2, &z, &w);
     //剰余が0ならば
     if (!isZero(&w)) {
-        struct Number m; clearByZero(&m);
+        struct Number m; copyByZero(&m);
         multiple(a, a, &m);
         fastpower(&m, &z, c);
         return 0;
     }
     struct Number s, n, m;
-    clearByZero(&s); clearByZero(&n); clearByZero(&m);
+    copyByZero(&s); copyByZero(&n); copyByZero(&m);
 
-    sub(b, &one, &s);
+    sub(b, &N1, &s);
     fastpower(a, &s, &n);
 
     multiple(a, &n, &m);
     copyNumber(&m, c);
 
+    return 0;
+}
+
+int shiftTo(int shift, struct Number *a){
+    a->n[shift] = 1;
+    return 0;
+}
+
+
+/**
+ * isOdd(struct Number *)
+ * 奇数 return 1
+ * 偶数 return 0
+ */
+int isOdd(struct Number *a){
+    if(a->n[0] % 2 != 0){
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * fastpowdiv(struct Number *, struct Number *, struct Number *, struct Number *, struct Number *)
+ * nmax : p = {2, 5, 8}，p^{2n} < RADIXを満たす最大のn
+ */
+struct Number memo[MEMO_SIZE];
+void fastpowdiv(struct Number *p, struct Number *q, struct Number *quo, struct Number *nmax, struct Number *a){
+    struct Number _, i; copyByZero(&i); copyByZero(&_);
+    struct Number _m; copyByZero(&_m);
+    struct Number n1, n2, _n2; copyByZero(&n1); copyByZero(&n2); copyByZero(&_n2);
+    // q < nmax
+    int _q = 0;
+    getInt(q, &_q);
+    if (isZero(&memo[_q]) != 0){
+        copyNumber(&memo[_q], &i);
+        fastdivide(quo, &i, a, &_);
+    } else if (numComp(q, nmax) == -1){
+        fastpower(p, q, &i);
+        copyNumber(&i, &memo[_q]);
+        fastdivide(quo, &memo[_q], a, &_);
+    } else {
+        if (isOdd(q)){
+            fastdivide(q, &N2, &n1, &_);
+            copyNumber(&n1, &_n2);
+            increment(&_n2, &n2);
+        } else {
+            fastdivide(q, &N2, &n1, &_);
+            copyNumber(&n1, &n2);
+        }
+        struct Number _a; copyByZero(&_a);
+        fastpowdiv(p, &n1, quo, nmax, &_a);
+        fastpowdiv(p, &n2, &_a, nmax, a);
+    }
+}
+
+void commonForArctan(int *n, struct Number *p, struct Number *a, struct Number *nmax){
+    struct Number _2n, q, h, i, j, k,
+                  quo, _,
+                  _n, _q, P2;
+
+    copyByZero(&_2n); copyByZero(&q);
+    copyByZero(&h); copyByZero(&i); copyByZero(&j); copyByZero(&k);
+    copyByZero(&quo); copyByZero(&_);
+    copyByZero(&_n);copyByZero(&_q);
+    copyByZero(&P2);
+
+    initNumber();
+
+    shiftTo(KETA-2, &h);
+    setInt(&_n, *n);
+    multiple(&_n, &N2, &_2n); // 2 * n
+    add(&_2n, &N1, &q); // 2n + 1 = q
+    fastdivide(&h, &q, &quo, &_); // h / (2n + 1)
+
+    fastdivide(&quo, p, a, &k); // (h / (2n + 1)) / p = a...k
+
+    fastpower(p, &N2, &P2); // p^2
+    fastpowdiv(&P2, &_n, a, nmax, &j); // a / ((p^2) ^ n) = j (n < nmax)
+    copyNumber(&j, a);
+
+    if(isOdd(&_n)){
+        setSign(a, -1);
+    } else {
+        setSign(a, 1);
+    }
+}
+
+
+/*
+  int arctan(struct Number *, struct Number *, struct Number *)
+
+  - q = 2n + 1
+  - 0 <= n <= INF
+
+  arctan(x) = (-1) ^ n / q * (x ^ q) for |x| < 1
+
+  実装の方針
+  x = 1/pとおく
+  arctan(1/p) = (-1)^ n / q * (p^q) for |1/p| < 1
+
+*/
+int arctan(struct Number *p, struct Number *a, struct Number *nmax){
+    /*
+     * old : 一つ前の項
+     * new : 次の項
+     */
+    struct Number old, new,
+                  sub_oldnew, abs_son, // abs_son : abs_sub_oldnew
+                  b, ans;
+
+    copyByZero(&old); copyByZero(&new);
+    copyByZero(&sub_oldnew); copyByZero(&abs_son);
+    copyByZero(&b); copyByZero(&ans);
+
+    // メモ化再帰用のメモ配列 初期化
+    for(int i = 0; i < MEMO_SIZE; i++){
+        copyByZero(&memo[i]);
+    }
+
+
+    for(int n = 0; ; n++) {
+        copyNumber(&new, &old);
+        commonForArctan(&n, p, a, nmax);
+        getAbs(a, &new);
+        sub(&old, &new, &sub_oldnew); getAbs(&sub_oldnew, &abs_son);
+        add(a, &ans, &b); copyNumber(&b, &ans);
+        if (numComp(&abs_son, &N1) == -1) break;
+    }
+    copyNumber(&ans, a);
     return 0;
 }
--- mulprec.h
+++ mulprec.h
@@ -1,11 +1,32 @@
-#define KETA 40
+#include<stdint.h>
+
+// 基数
+#define RADIX 1000000000
+#define RADIX_KETA (int)log10(RADIX)
+
+ /**
+  * KETA = 求めたい桁数 / 9
+  *
+  * NOTE: 後ろ数桁は誤差がある
+  */
+#ifdef PI
+#define KETA 114 // 1000桁程度
+//#define KETA 556 // 5000桁程度
+//#define KETA 1114 // 10000桁程度
+#endif
+
+// for testing
+#ifdef TEST
+#define KETA 20
+#endif
 
 struct Number {
-    int n[KETA];
+    uint_fast64_t n[KETA];
     int sign;
 };
 
 void clearByZero(struct Number *);
+void copyByZero(struct Number *);
 void putSign(struct Number *);
 void setRnd(struct Number *, int);
 void dispNumber(struct Number *);
@@ -16,6 +37,8 @@ int isZero(struct Number *);
 int mulBy10(struct Number *, struct Number *);
 int divBy10(struct Number *, struct Number *);
 int setInt(struct Number *, int);
+int setIntToZero(struct Number *, int);
+int getInt(struct Number *, int *);
 int setSign(struct Number *, int);
 int getSign(struct Number *);
 int numComp(struct Number *, struct Number *);
@@ -25,5 +48,8 @@ int simpleMultiple(int, int, int *);
 int multiple(struct Number *, struct Number *, struct Number *);
 int increment(struct Number *, struct Number *);
 int divide(struct Number *, struct Number *, struct Number *, struct Number *);
+int fastdivide(struct Number *, struct Number *, struct Number *, struct Number *);
 int power(struct Number *, struct Number *, struct Number *);
 int fastpower(struct Number *, struct Number *, struct Number *);
+int arctan(struct Number *, struct Number *, struct Number *);
+
